a306baab59894ce8346456d2b7cc35d49c86eb93
Kirill Kondrenko <kondrenkokp@yandex.ru>
2023-10-08 00:11:01 +0700

    Fix to 1.6 of OS

--- Bachelor/Year-3/OS/threads/4/mythread/my_thread_data.c
+++ Bachelor/Year-3/OS/threads/4/mythread/my_thread_data.c
@@ -1,10 +1,10 @@
-#include <sys/mman.h>
-#include <malloc.h>
+#include <stdio.h>
 #include <string.h>
+#include "utils.h"
 #include "my_thread_data.h"
 
 void free_mythread_data(struct mythread_data_t* mythread_data) {
     int id = mythread_data->id;
-    const int code = munmap(mythread_data->stack,  mythread_data->stack_size);
+    const int code = free_mmap_region(mythread_data->stack, mythread_data->stack_size);
     printf("Resources for thread-%d are freed with %s\n", id, strerror(code));
 }
